dba9898b9dd68813efe96dd76856c16a688a0718
Martin Nowak <code@dawg.eu>
2015-02-17 20:42:40 +0100

    script to deploy docs from Travis-CI builds

---
+++ travis.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+if [ "$COVERAGE" = true ]; then
+    dub fetch doveralls
+    dub test -b unittest-cov --compiler=${DC}
+    dub run doveralls --compiler=${DC}
+else
+    dub test --compiler=${DC}
+fi
+
+if [ ! -z "$GH_TOKEN" ]; then
+    dub build -b ddox --compiler=${DC}
+    cd docs
+    git init
+    git config user.name "Travis-CI"
+    git config user.email "travis@nodemeatspace.com"
+    git add .
+    git commit -m "Deployed to Github Pages"
+    git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}" master:gh-pages > /dev/null 2>&1
+fi
